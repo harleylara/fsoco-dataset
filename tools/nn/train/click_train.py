d39f572dc38a97450426c241eb61419b3baabab6
Michael Schötz <schoetz.michael@gmail.com>
2022-01-14 20:16:13 +0100

    added option to pass kwargs

--- tools/nn/train/click_train.py
+++ tools/nn/train/click_train.py
@@ -1,18 +1,45 @@
 import click
 
+from utils.logger import Logger
+
 from .train import main
 
 
-@click.command()
+@click.command(
+    context_settings=dict(
+        ignore_unknown_options=True,
+        allow_extra_args=True,
+    )
+)
 @click.argument("sly_project_folder_train", type=str)
-@click.argument("output_folder", type=str)
-@click.option("--config", type=str, default=False)
-def train(sly_project_folder_train, output_folder, config):
+@click.argument("working_folder", type=str)
+@click.option("--test_split", type=click.FloatRange(0, 1.0, clamp=True))
+@click.pass_context
+def train(ctx, sly_project_folder_train, working_folder, test_split):
     """
     #TODO add documentation
 
     """
-    main(sly_project_folder_train, output_folder, config)
+
+    kwargs = {}
+    new_key = True
+    key = ""
+    for item in ctx.args:
+        is_key = item.startswith("--")
+        if is_key and new_key:
+            key = item.strip("--")
+            new_key = False
+        elif is_key and not new_key:
+            # key direct after key -> Flag
+            kwargs[key] = True
+        elif not is_key and not new_key:
+            # value after key
+            kwargs[key] = item
+            new_key = True
+        else:
+            Logger.log_warn(f"unkown argument '{item}'")
+
+    main(sly_project_folder_train, working_folder, test_split, kwargs)
 
 
 if __name__ == "__main__":
--- tools/nn/train/train.py
+++ tools/nn/train/train.py
@@ -1,2 +1,32 @@
-def main(sly_project_folder_train, output_folder, config):
-    print("train")
+# from yolov5 import train
+from pathlib import Path
+
+from utils.logger import Logger
+
+
+class FSOCOTrainer:
+    def __init__(
+        self,
+        sly_project_folder_train: str,
+        working_folder: str,
+        test_split: float,
+        train_kwargs: dict,
+    ) -> None:
+
+        self._working_dir = self._init_working_folder(working_folder)
+
+    def _init_working_folder(self, working_folder: str) -> None:
+        work_dir = Path(working_folder)
+        work_dir.mkdir(parents=True, exist_ok=True)
+
+        Logger.log_info(f"Using working directory - {work_dir}")
+
+    def train(self):
+        pass
+
+
+def main(sly_project_folder_train, working_folder, test_split, train_kwargs):
+    trainer = FSOCOTrainer(
+        sly_project_folder_train, working_folder, test_split, train_kwargs
+    )
+    trainer.train()
